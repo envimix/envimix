70cea6d6feb3b4328f46c78ef2c78a37735427ee
Petr Pivoňka <petrpiv1@gmail.com>
2023-09-26 01:53:30 +0200

    Add hardcoded allowed login list

--- Envimix/Scripts/MainMenu.cs
+++ Envimix/Scripts/MainMenu.cs
@@ -1,9 +1,18 @@
-﻿namespace Envimix.Scripts;
+﻿using System.Collections.Immutable;
+
+namespace Envimix.Scripts;
 
 public class MainMenu : CManiaAppTitle, IContext
 {
     public void Main()
     {
+        ImmutableArray<string> allowedLogins = new() { "bigbang1112", "linuxcat", "pekatour", "adamkooo", "poutrel" };
+
+        if (!allowedLogins.Contains(LocalUser.Login))
+        {
+            return;
+        }
+
         var layer = UILayerCreate();
         layer.ManialinkPage = "file://Media/Manialinks/MainMenu.xml";
     }
