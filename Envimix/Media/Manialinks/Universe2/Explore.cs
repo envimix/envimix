424c68670c0358afbcd4659464e24dec668114a2
Petr Pivoňka <petrpiv1@gmail.com>
2023-10-11 12:17:07 +0200

    Add some form of zooming on start

--- Envimix/Media/Manialinks/Universe2/Explore.cs
+++ Envimix/Media/Manialinks/Universe2/Explore.cs
@@ -3,6 +3,7 @@
 public class Explore : CMapEditorPluginLayer, IContext
 {
     [ManialinkControl] public required CMlQuad QuadTest;
+    [ManialinkControl] public required CMlQuad QuadStart;
     [ManialinkControl] public required CMlQuad QuadStartIcon;
 
     public bool Testing;
@@ -14,19 +15,43 @@ public class Explore : CMapEditorPluginLayer, IContext
             Testing = !Testing;
             QuadTest.StyleSelected = Testing;
         };
+
+        QuadStart.MouseClick += () =>
+        {
+            MoveToStart();
+        };
     }
 
     public void Main()
     {
+        
+    }
+
+    private void MoveToStart()
+    {
+        Vec3 startPos = new(0, 0, 0);
+
         foreach (var block in Editor.Blocks)
         {
             if (block.BlockModel.WaypointType == CBlockModel.EWayPointType.Start)
             {
-                Log(block);
                 QuadStartIcon.Image = block.BlockModel.Icon;
+                startPos = Editor.GetVec3FromCoord(block.Coord);
                 break;
             }
         }
+
+        foreach (var anchor in Editor.AnchorData)
+        {
+            if (anchor.WaypointType == CAnchorData.EWaypointType.Start)
+            {
+                startPos = anchor.Position;
+                break;
+            }
+        }
+
+        Editor.CameraTargetPosition = startPos;
+        Editor.Camera.Zoom(CMapEditorCamera.EZoomLevel.Close, Smooth: true);
     }
 
     public void Loop()
