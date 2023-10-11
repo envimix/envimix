6f5418f9f421dcca1b05d671246b2b5b26a3acf0
Petr Pivoňka <petrpiv1@gmail.com>
2023-10-12 01:36:45 +0200

    Add more interactivity to Explore

--- Envimix/Media/Manialinks/Universe2/Explore.cs
+++ Envimix/Media/Manialinks/Universe2/Explore.cs
@@ -1,12 +1,23 @@
-﻿namespace Envimix.Media.Manialinks.Universe2;
+﻿using System.Collections.Immutable;
+
+namespace Envimix.Media.Manialinks.Universe2;
 
 public class Explore : CMapEditorPluginLayer, IContext
 {
     [ManialinkControl] public required CMlQuad QuadTest;
     [ManialinkControl] public required CMlQuad QuadStart;
     [ManialinkControl] public required CMlQuad QuadStartIcon;
+    [ManialinkControl] public required CMlLabel LabelStart;
+    [ManialinkControl] public required CMlQuad QuadSwitchEditor;
+    [ManialinkControl] public required CMlLabel LabelSwitchEditor;
+    [ManialinkControl] public required CMlFrame FrameExplore;
+    [ManialinkControl] public required CMlLabel LabelCp;
+    [ManialinkControl] public required CMlQuad QuadCpIcon;
+    [ManialinkControl] public required CMlLabel LabelFinish;
+    [ManialinkControl] public required CMlQuad QuadFinishIcon;
 
     public bool Testing;
+    public bool NormalEditor;
 
     public Explore()
     {
@@ -14,6 +25,24 @@ public class Explore : CMapEditorPluginLayer, IContext
         {
             Testing = !Testing;
             QuadTest.StyleSelected = Testing;
+        };
+
+        QuadSwitchEditor.MouseClick += () =>
+        {
+            NormalEditor = !NormalEditor;
+            QuadSwitchEditor.StyleSelected = NormalEditor;
+            Editor.HideEditorInterface = !NormalEditor;
+
+            if (NormalEditor)
+            {
+                LabelSwitchEditor.SetText("Switch to Explore mode");
+            }
+            else
+            {
+                LabelSwitchEditor.SetText("Switch to normal editor");
+            }
+
+            FrameExplore.Visible = !NormalEditor;
         };
 
         QuadStart.MouseClick += () =>
@@ -30,12 +59,11 @@ public class Explore : CMapEditorPluginLayer, IContext
     private void MoveToStart()
     {
         Vec3 startPos = new(0, 0, 0);
-
+        
         foreach (var block in Editor.Blocks)
         {
             if (block.BlockModel.WaypointType == CBlockModel.EWayPointType.Start)
             {
-                QuadStartIcon.Image = block.BlockModel.Icon;
                 startPos = Editor.GetVec3FromCoord(block.Coord);
                 break;
             }
@@ -56,6 +84,46 @@ public class Explore : CMapEditorPluginLayer, IContext
 
     public void Loop()
     {
+        QuadStartIcon.Hide();
+        LabelStart.Show();
+
+        var start = "";
+        ImmutableArray<CBlock> cps = new();
+        ImmutableArray<CBlock> finishes = new();
+
+        foreach (var block in Editor.Blocks)
+        {
+            if (block.BlockModel.WaypointType == CBlockModel.EWayPointType.Start && start is "")
+            {
+                QuadStartIcon.Show();
+                QuadStartIcon.Image = block.BlockModel.Icon;
+                LabelStart.Hide();
+                start = block.BlockModel.Name;
+            }
+            else if (block.BlockModel.WaypointType == CBlockModel.EWayPointType.Checkpoint)
+            {
+                if (cps.Length == 0)
+                {
+                    QuadCpIcon.Show();
+                    QuadCpIcon.Image = block.BlockModel.Icon;
+                    LabelCp.Hide();
+                }
+
+                cps.Add(block);
+            }
+            else if (block.BlockModel.WaypointType == CBlockModel.EWayPointType.Finish)
+            {
+                if (finishes.Length == 0)
+                {
+                    QuadFinishIcon.Show();
+                    QuadFinishIcon.Image = block.BlockModel.Icon;
+                    LabelFinish.Hide();
+                }
+
+                finishes.Add(block);
+            }
+        }
+
         if (Testing)
         {
             Editor.PlaceModeE = CMapEditorPlugin.PlaceMode.Test;
