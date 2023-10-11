a4abc7dd51c08ae8dc0308f033f5f26bb33b736a
Petr Pivoňka <petrpiv1@gmail.com>
2023-10-11 02:18:52 +0200

    Add WIP Explore implementation

---
+++ Envimix/Media/Manialinks/Universe2/Explore.cs
@@ -0,0 +1,45 @@
+﻿namespace Envimix.Media.Manialinks.Universe2;
+
+public class Explore : CMapEditorPluginLayer, IContext
+{
+    [ManialinkControl] public required CMlQuad QuadTest;
+    [ManialinkControl] public required CMlQuad QuadStartIcon;
+
+    public bool Testing;
+
+    public Explore()
+    {
+        QuadTest.MouseClick += () =>
+        {
+            Testing = !Testing;
+            QuadTest.StyleSelected = Testing;
+        };
+    }
+
+    public void Main()
+    {
+        foreach (var block in Editor.Blocks)
+        {
+            if (block.BlockModel.WaypointType == CBlockModel.EWayPointType.Start)
+            {
+                Log(block);
+                QuadStartIcon.Image = block.BlockModel.Icon;
+                break;
+            }
+        }
+    }
+
+    public void Loop()
+    {
+        if (Testing)
+        {
+            Editor.PlaceModeE = CMapEditorPlugin.PlaceMode.Test;
+            Editor.EditModeE = CMapEditorPlugin.EditMode.Place;
+        }
+        else
+        {
+            Editor.PlaceModeE = CMapEditorPlugin.PlaceMode.Block;
+            Editor.EditModeE = CMapEditorPlugin.EditMode.FreeLook;
+        }
+    }
+}
--- Envimix/Scripts/MapTypes/TrackMania/EnvimixExplore.cs
+++ Envimix/Scripts/MapTypes/TrackMania/EnvimixExplore.cs
@@ -2,9 +2,35 @@
 
 public class EnvimixExplore : CTmMapType, IContext
 {
+    public EnvimixExplore()
+    {
+        StartTest += () =>
+        {
+            TestMapWithMode("Scripts/Modes/TrackMania/EnvimixSolo.Script.txt");
+        };
+    }
+
+    public string ReadFile(string fileName)
+    {
+        var request = Http.CreateGet("file://Media/" + fileName);
+        Wait(() => request.IsCompleted);
+
+        var result = request.Result;
+        if (result == "")
+        {
+            Log("Warning: File located in file://Media/" + fileName + " does not exist or is empty.");
+        }
+
+        Http.Destroy(request);
+        return result;
+    }
+
     public void Main()
     {
+        HideEditorInterface = true;
+        EnableMapTypeStartTest = true;
 
+        UILayerCreate().ManialinkPage = ReadFile("Manialinks/Universe2/Explore.xml");
     }
 
     public void Loop()
