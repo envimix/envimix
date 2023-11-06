9d09ea4586a8407fcf7c8331ff007ca69014e2fb
Petr Pivoňka <petrpiv1@gmail.com>
2023-11-06 10:42:28 +0100

    Fix size

--- Envimix/Media/ManiaApps/EnvimixSingleplayerClient.cs
+++ Envimix/Media/ManiaApps/EnvimixSingleplayerClient.cs
@@ -74,8 +74,8 @@ public class EnvimixSingleplayerClient : CManiaAppPlayground, IContext
             vehicleManialink = $"{vehicleManialink}        <label pos=\"0 -0.5\" z-index=\"2\" size=\"70 10\" text=\"{vehicle}\" halign=\"center\" valign=\"center2\" textsize=\"6\" textfont=\"RajdhaniMono\" id=\"LabelVehicle\"/>";
             vehicleManialink = $"{vehicleManialink}        <label pos=\"37.5 -8\" z-index=\"2\" size=\"75 5\" text=\"Default\" textprefix=\"$t\" halign=\"right\" valign=\"bottom\" textfont=\"Oswald\" textsize=\"2\" textcolor=\"FF0\" id=\"LabelDefault\" translate=\"1\" hidden=\"1\"/>";
             vehicleManialink = $"{vehicleManialink}        <quad pos=\"35 5\" z-index=\"3\" size=\"7.5 7.5\" halign=\"center\" valign=\"center\" style=\"BgRaceScore2\" substyle=\"Fame\" id=\"QuadStar\" hidden=\"1\"/>";
-            vehicleManialink = $"{vehicleManialink}        <gauge id=\"GaugeDifficulty\" pos=\"-40 -5\" z-index=\"3\" size=\"11 6.5\" drawbg=\"0\" valign=\"center\" ratio=\"0\"/>";
-            vehicleManialink = $"{vehicleManialink}        <gauge id=\"GaugeQuality\" pos=\"-40 -7\" z-index=\"3\" size=\"11 6.5\" drawbg=\"0\" valign=\"center\" ratio=\"0\"/>";
+            vehicleManialink = $"{vehicleManialink}        <gauge id=\"GaugeDifficulty\" pos=\"-40 -5\" z-index=\"3\" size=\"0 6.5\" drawbg=\"0\" valign=\"center\" ratio=\"0\"/>";
+            vehicleManialink = $"{vehicleManialink}        <gauge id=\"GaugeQuality\" pos=\"-40 -7\" z-index=\"3\" size=\"0 6.5\" drawbg=\"0\" valign=\"center\" ratio=\"0\"/>";
             vehicleManialink = $"{vehicleManialink}        <quad id=\"QuadFlash\" z-index=\"4\" size=\"80 19\" valign=\"center\" halign=\"center\" style=\"Bgs1\" substyle=\"BgWindow4\" opacity=\"0\"/>";
             vehicleManialink = $"{vehicleManialink}     </frame>";
         }
