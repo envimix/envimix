ebf5a3fa7c9ca884e81407177febcf2fc327cfe9
Petr Pivoňka <petrpiv1@gmail.com>
2025-10-27 20:52:24 +0100

    Dont show endscreen in explore, add more results

--- Envimix/Media/Manialinks/Universe2/Endscreen.cs
+++ Envimix/Media/Manialinks/Universe2/Endscreen.cs
@@ -10,11 +10,14 @@ public class Endscreen : CTmMlScriptIngame, IContext
     [ManialinkControl] public required CMlFrame FrameLeaderboard;
     [ManialinkControl] public required CMlFrame FrameContinue;
     [ManialinkControl] public required CMlFrame FrameLabelMapName;
+    [ManialinkControl] public required CMlFrame FrameLabelCar;
     [ManialinkControl] public required CMlLabel LabelAuthor;
     [ManialinkControl] public required CMlLabel LabelEnvironment;
     [ManialinkControl] public required CMlQuad QuadEnvironment;
+    [ManialinkControl] public required CMlLabel LabelTime;
 
     public int FinishedAt;
+    public string PreviousCar = "";
 
     public Endscreen()
     {
@@ -23,9 +26,10 @@ public class Endscreen : CTmMlScriptIngame, IContext
             switch (e.Type)
             {
                 case CTmRaceClientEvent.EType.WayPoint:
-                    if (e.IsEndRace)
+                    if (e.IsEndRace && !IsExplore())
                     {
                         FinishedAt = Now;
+                        LabelTime.SetText(TimeToTextWithMilli(e.RaceTime));
                         ShowEndscreen();
                         
                         // show full endscreen in 2 seconds, hide the other UI after the 2 seconds, move blur background from bottom to top for interesting effect
@@ -67,13 +71,29 @@ public class Endscreen : CTmMlScriptIngame, IContext
         return true;
     }
 
+    bool IsExplore()
+    {
+        return CurrentServerModeName is "";
+    }
+
+    static string TimeToTextWithMilli(int time)
+    {
+        var formatted = $"{TextLib.TimeToText(time, true)}{MathLib.Abs(time % 10)}";
+        if (TextLib.Length(TextLib.Split(".", formatted)[1]) > 3)
+            return TextLib.SubString(formatted, 0, TextLib.Length(formatted) - 1);
+        return formatted;
+    }
+
     public void Main()
     {
         HideEndscreen();
+        FrameEndscreenInfo.Hide();
         SetSlidingText(FrameLabelMapName, Map.MapInfo.Name);
         LabelAuthor.Value = Map.MapInfo.AuthorNickName;
         LabelEnvironment.Value = Map.CollectionName;
         QuadEnvironment.ChangeImageUrl($"file://Media/Images/Environments/{Map.CollectionName}.png");
+
+        Page.GetClassChildren("LOADING", Page.MainFrame, true);
 
         Wait(() => GetPlayer() is not null);
     }
@@ -83,33 +103,53 @@ public class Endscreen : CTmMlScriptIngame, IContext
         if (FinishedAt != -1)
         {
             MoveSlidingText(FrameLabelMapName, 10, 0.01f);
+            MoveSlidingText(FrameLabelCar, 10, -0.01f);
 
             if (Input.IsKeyPressed(109) || Input.IsKeyPressed(119))
             {
                 Continue();
             }
         }
+
+        var car = Netread<string>.For(GetPlayer());
+
+        if (car.Get() != PreviousCar)
+        {
+            PreviousCar = car.Get();
+            SetSlidingText(FrameLabelCar, car.Get());
+        }
+
+        foreach (var control in Page.GetClassChildren_Result)
+        {
+            if (control.Visible)
+            {
+                control.RelativeRotation += Period * 0.2f;
+            }
+        }
     }
 
     void ShowEndscreen()
     {
-        FrameEndscreenInfo.Show();
+        FrameEndscreenInfo.RelativePosition_V3.X = -40;
+        FrameEndscreenInfo.RelativeScale = 1;
+        AnimMgr.Add(FrameEndscreenInfo, "<frame pos=\"0 0\" hidden=\"0\" />", 500, CAnimManager.EAnimManagerEasing.QuadOut);
         AnimMgr.Add(QuadBlur, "<quad scale=\"1\" hidden=\"0\" />", 500, CAnimManager.EAnimManagerEasing.QuadOut);
-        AnimMgr.Add(FrameTime, "<frame pos=\"0 0\" />", 600, CAnimManager.EAnimManagerEasing.QuadOut);
-        AnimMgr.Add(FrameScore, "<frame pos=\"0 0\" />", 500, CAnimManager.EAnimManagerEasing.QuadOut);
-        AnimMgr.Add(FrameLeaderboard, "<frame pos=\"0 0\" />", 400, CAnimManager.EAnimManagerEasing.QuadOut);
-        AnimMgr.Add(FrameContinue, "<frame pos=\"0 0\" />", Now + 400, 500, CAnimManager.EAnimManagerEasing.QuadOut);
+        AnimMgr.Add(FrameTime, "<frame pos=\"0 0\" />", Now + 200, 600, CAnimManager.EAnimManagerEasing.QuadOut);
+        AnimMgr.Add(FrameScore, "<frame pos=\"0 0\" />", Now + 200, 500, CAnimManager.EAnimManagerEasing.QuadOut);
+        AnimMgr.Add(FrameLeaderboard, "<frame pos=\"0 0\" />", Now + 200, 400, CAnimManager.EAnimManagerEasing.QuadOut);
+        FrameContinue.RelativeScale = 1;
+        AnimMgr.Add(FrameContinue, "<frame pos=\"0 0\" />", Now + 600, 500, CAnimManager.EAnimManagerEasing.QuadOut);
     }
 
     void HideEndscreen()
     {
         FinishedAt = -1;
-        FrameEndscreenInfo.Hide();
-        AnimMgr.Add(QuadBlur, "<quad scale=\"0\" hidden=\"1\" />", 500, CAnimManager.EAnimManagerEasing.QuadOut);
-        FrameTime.RelativePosition_V3.X = 140;
-        FrameScore.RelativePosition_V3.X = 140;
-        FrameLeaderboard.RelativePosition_V3.X = -60;
-        FrameContinue.RelativePosition_V3.Y = 21;
+        AnimMgr.Add(QuadBlur, "<quad scale=\"0\" hidden=\"1\" />", 500, CAnimManager.EAnimManagerEasing.QuadIn);
+        AnimMgr.Add(FrameEndscreenInfo, "<frame pos=\"-10 0\" scale=\"0.2\" hidden=\"1\" />", 250, CAnimManager.EAnimManagerEasing.QuadIn);
+        AnimMgr.Add(FrameTime, "<frame pos=\"140 0\" />", 250, CAnimManager.EAnimManagerEasing.QuadIn);
+        AnimMgr.Add(FrameScore, "<frame pos=\"140 0\" />", 250, CAnimManager.EAnimManagerEasing.QuadIn);
+        AnimMgr.Add(FrameLeaderboard, "<frame pos=\"-60 0\" />", 250, CAnimManager.EAnimManagerEasing.QuadIn);
+        AnimMgr.Add(FrameContinue, "<frame pos=\"0 21\" scale=\"0\" />", 250, CAnimManager.EAnimManagerEasing.QuadIn);
     }
 
     private void Continue()
