d2225977e69a0b05901780e4e95821cbc943e90d
Petr Pivoňka <petrpiv1@gmail.com>
2025-12-07 23:53:21 +0100

    Fix login issue on clientside

--- Envimix/Scripts/MainMenu.cs
+++ Envimix/Scripts/MainMenu.cs
@@ -618,12 +618,17 @@ public class MainMenu : CManiaAppTitle, IContext
                 Log("User token creation failed (JSON issue).");
                 Log(UserTokenRequest.Result);
             }
-            else if (response.Login != LocalUser.Login)
+            else if (response.Login != TextLib.ToLowerCase(LocalUser.Login))
             {
                 Log("User token creation failed (server login mismatch).");
             }
             else
             {
+                if (response.Login != LocalUser.Login)
+                {
+                    Log($"Warning: login case mismatch (local: {LocalUser.Login}, server: {response.Login}), should be fine, but it's weird");
+                }
+
                 EnvimixTurboUserIsAdmin = response.IsAdmin;
                 if (response.IsAdmin)
                 {
