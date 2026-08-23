e8088a708c46ae37ae6111044988d86900337e56
Petr Pivoňka <petrpiv1@gmail.com>
2026-08-23 02:56:55 +0200

    Add wip ScoreboardTimeAttack

---
+++ Envimix/Media/Manialinks/Universe2/ScoreboardTimeAttack.cs
@@ -0,0 +1,14 @@
+﻿namespace Envimix.Media.Manialinks.Universe2;
+
+public class ScoreboardTimeAttack : CTmMlScriptIngame, IContext
+{
+    public void Main()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Loop()
+    {
+        throw new NotImplementedException();
+    }
+}
